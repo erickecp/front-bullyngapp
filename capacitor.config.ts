3112a279cdb9599c46944898b8c841a40f47881e
erickecp <43622490+erickecp@users.noreply.github.com>
2023-04-05 17:15:49 -0600

    todos los cambios

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bullyngApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
